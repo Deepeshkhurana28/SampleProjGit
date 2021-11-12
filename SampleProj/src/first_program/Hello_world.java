45bbba1a1cdd33a51bdce3942d9e1b20b7994d6c
Administrator <Administrator@LIN80032073.corp.capgemini.com>
2021-11-12 13:19:33 +0530

    Test commit

---
+++ SampleProj/src/first_program/Hello_world.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package first_program;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Hello_world {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World");
+
+	}
+
+}
